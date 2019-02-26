b78e31d78c0fed6d7c5760c7da307acba7230664
Mateo Anzorena <manzorena@mstech.la>
2019-02-26 17:10:30 -0400

    primer commit

---
+++ src/webparts/almuerzo/components/IAlmuerzoProps.ts
@@ -0,0 +1,3 @@
+export interface IAlmuerzoProps {
+  description: string;
+}
---
+++ src/webparts/almuerzo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAlmuerzoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AlmuerzoWebPartStrings' {
+  const strings: IAlmuerzoWebPartStrings;
+  export = strings;
+}
