9eb92390448f1f157c7ab825e677416debe8980c
Mateo Anzorena <manzorena@mstech.la>
2019-03-07 10:21:20 -0300

    fix JSON

--- src/webparts/almuerzo/components/Order/Order.tsx
+++ src/webparts/almuerzo/components/Order/Order.tsx
@@ -8,6 +8,7 @@ import Selector_menu from '../Selector/Selector_menu';
 import _Checkbox from '../Checkbox/Checkbox';
 import { Checkbox } from 'office-ui-fabric-react/lib/Checkbox';
 import Gustos from '../Gustos/Gustos';
+import Checkbox_ from '../Checkbox/Checkbox';
 
 
 var menujson = require('./lista_menus.json');
@@ -34,17 +35,64 @@ class Order extends React.Component<{},{}> {
                 <div id="hidden">
 
                     <div id="carne">
+                        {console.log(Menus[0]["Tipo"])}
+                        {console.log(Menus[0]["Guarnición"])}
                         <Selector label="Tipo"       menuarr={Menus[0]["Tipo"]}/>
                         <Selector label="Guarnición" menuarr={Menus[0]["Guarnición"]}/>
                     </div>
 
                     <div id="Ensalada para armar">
+                       {console.log(Menus[1]["Gusto especial"])}
                         <Selector label="Gusto especial" menuarr={Menus[1]["Gusto especial"]}/>
                         <Gustos  gustos={Menus[1]["Gustos"]}/>
                     </div>
-                
-                
-                
+
+                    <div id="Omelette con guarnición">
+                        {console.log(Menus[2]["Tipo"])}
+                        {console.log(Menus[2]["Guarnición"])}
+                        <Selector label="Tipo"       menuarr={Menus[2]["Tipo"]}/>
+                        <Selector label="Guarnición" menuarr={Menus[2]["Guarnición"]}/>
+                    </div>
+
+                    <div id="Rolls">
+                       {console.log(Menus[3]["Tipo"])}
+
+                        <Selector label="Tipo"       menuarr={Menus[3]["Tipo"]}/>
+                    </div>
+
+                    <div id="Pizzetas de arroz (para celíacos)">
+                    {console.log(Menus[4]["Tipo"])}
+
+                        <Selector label="Tipo"       menuarr={Menus[4]["Tipo"]}/>
+                    </div>
+
+                    <div id="Tartas con guarnición">
+                        {console.log(Menus[5]["Tipo"])}
+                        {console.log(Menus[5]["Guarnición"])}
+                        <Selector label="Tipo"       menuarr={Menus[5]["Tipo"]}/>
+                        <Selector label="Guarnición" menuarr={Menus[5]["Guarnición"]}/>
+                    </div>
+
+                    <div id="Milanesas vegetarianas">
+                        {console.log(Menus[6]["Tipo"])}
+                        {console.log(Menus[6]["Guarnición"])}
+                        <Selector label="Tipo"       menuarr={Menus[6]["Tipo"]}/>
+                        <Selector label="Guarnición" menuarr={Menus[6]["Guarnición"]}/>
+                        <Checkbox_ label="Gratinada"/>
+                    </div>
+
+                    <div id="Sándwich">
+                    {console.log(Menus[7]["Tipo"])}
+
+                        <Selector label="Tipo"       menuarr={Menus[7]["Tipo"]}/>
+                    </div>
+
+                    <div id="Pasta">
+                         {console.log(Menus[8]["Tipo"])}
+                         {console.log(Menus[8])}
+                         {console.log(Menus[8]["Salsa"])}
+                        <Selector label="Tipo"       menuarr={Menus[8]["Tipo"]}/>
+                    </div>
                 </div>
 
                 <ActionButton>BOTON</ActionButton>
