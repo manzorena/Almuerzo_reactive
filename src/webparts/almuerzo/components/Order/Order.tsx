57c230d9799613601a3c3b5422a469794592826e
Mateo <manzorena@mstech.la>
2019-03-13 10:08:47 -0400

    terminado el front de la interface de pedido

--- src/webparts/almuerzo/components/Order/Order.tsx
+++ src/webparts/almuerzo/components/Order/Order.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
-import { Dropdown, IDropdown, DropdownMenuItemType, IDropdownOption } from 'office-ui-fabric-react/lib/Dropdown';
 import {ActionButton, IButtonProps} from 'office-ui-fabric-react/lib/Button';
 import styles from './Order.module.scss';
 import '../Almuerzo';
 import Selector from '../Selector/Selector';
 import Selector_menu from '../Selector/Selector_menu';
-import _Checkbox from '../Checkbox/Checkbox';
-import { Checkbox } from 'office-ui-fabric-react/lib/Checkbox';
 import Rowgustos from '../Gustos/Gustos';
 import Checkbox_ from '../Checkbox/Checkbox';
-import { HtmlAttributes } from '../../../../../node_modules/csstype';
 
-
+//importa el archivo json con todos los datos de las comidas
 var menujson = require('./lista_menus.json');
 
 var Menus = menujson["menus"];
@@ -26,7 +22,8 @@ var Oficinas = menujson["oficinas"];
 class Order extends React.Component<{},{}> {
 
     render() {
-        
+    
+
         return(
             <div>
                 <div id="Menu_principal" onFocus={a => this.showme(a)}>
@@ -34,58 +31,50 @@ class Order extends React.Component<{},{}> {
                 </div>
 
                 <div id="hidden">
-                    <div id={Menus[0]["Menu"]} className={styles.hidden}>
-                        <Selector label="Tipo"       menuarr={Menus[0]["Tipo"]}/>
-                        <Selector label="Guarnición" menuarr={Menus[0]["Guarnición"]}/>
-                    </div>
+                    {console.log("antes de menu")}
+                    {Menus.map((el,index)=>
 
-                    <div id={Menus[1]["Menu"]} className={styles.hidden}>
-                        <Selector label="Gusto especial" menuarr={Menus[1]["Gusto especial"]}/>
-                        <Rowgustos  gustos={Menus[1]["Gustos"]}/>
-                    </div>
+                    <div id={el["Menu"]} className={styles.hidden}> 
+                        {this.create_options(el)}
+                    </div>)}
+                </div>
 
-                    <div id={Menus[2]["Menu"]} className={styles.hidden}>
-                        <Selector label="Tipo"       menuarr={Menus[2]["Tipo"]}/>
-                        <Selector label="Guarnición" menuarr={Menus[2]["Guarnición"]}/>
-                    </div>
-
-                    <div id={Menus[3]["Menu"]} className={styles.hidden}>
-                        <Selector label="Tipo"       menuarr={Menus[3]["Tipo"]}/>
-                    </div>
-
-                    <div id={Menus[4]["Menu"]} className={styles.hidden}>
-                        <Selector label="Tipo"       menuarr={Menus[4]["Tipo"]}/>
-                    </div>
-
-                    <div id={Menus[5]["Menu"]} className={styles.hidden}>
-                        <Selector label="Tipo"       menuarr={Menus[5]["Tipo"]}/>
-                        <Selector label="Guarnición" menuarr={Menus[5]["Guarnición"]}/>
-                    </div>
-
-                    <div id={Menus[6]["Menu"]} className={styles.hidden}>
-                        <Selector  label="Tipo"       menuarr={Menus[6]["Tipo"]}/>
-                        <Selector  label="Guarnición" menuarr={Menus[6]["Guarnición"]}/>
-                        <Checkbox_ label="Gratinada"/>
-                    </div>
-
-                    <div id={Menus[7]["Menu"]} className={styles.hidden}>
-                        <Selector label="Tipo"       menuarr={Menus[7]["Tipo"]}/>
-                    </div>
-
-                    <div id={Menus[8]["Menu"]} className={styles.hidden}>
-                        <Selector label="Tipo"       menuarr={Menus[8]["Tipo"]}/>
-                        <Selector label="Salsa"      menuarr={Menus[8]["Salsa"]}/>
-                    </div>
-
-                    <div id={Menus[9]["Menu"]} className={styles.hidden}>
-                    </div>
+                <div id="oficinas">
+                <Selector label="Enviar pedido a" menuarr={Oficinas}/>
                 </div>
 
                 <ActionButton>BOTON</ActionButton>
             </div>
         )
-
     }
+
+
+    public create_options(obj){
+        {console.log("en create options")}
+        let output= [];
+
+        obj["Props"].map((el,i)=>
+        {switch (el) {
+
+            case "Gustos":
+                output[i] = <Rowgustos  gustos={obj[el]}/>
+                break;
+                
+            case "Gratinada":
+                output[i] = <Checkbox_ label={el}/>
+                break;
+
+            default:
+                output[i] = <Selector label={el} menuarr={obj[el]}/>
+                break;           
+        }}
+    )
+        console.log(output);
+        
+        return output;
+    }
+
+
 
     private _log(str: string): () => void {
         return (): void => {
